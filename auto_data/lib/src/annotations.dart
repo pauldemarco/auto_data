9e63f8a586d70cb00ad4086a7bdb3a415c805a90
Paul DeMarco <paulmdemarco@gmail.com>
2019-07-24 18:20:29 -0400

    Fix comments

--- auto_data/lib/src/annotations.dart
+++ auto_data/lib/src/annotations.dart
@@ -5,29 +5,21 @@
 part of auto_data;
 
 class Data {
-  /**
-   * Mark a class for data generation
-   */
+  /// Mark a class for data generation
   const Data();
 
   String toString() => "Data class";
 }
 
-/**
- * Marks a class as [Data].
- */
+/// Marks a class as [Data].
 const Data data = const Data();
 
 class Nullable {
-  /**
-   * Allow a field to be nullable
-   */
+  /// Allow a field to be nullable
   const Nullable();
 
   String toString() => "Nullable class";
 }
 
-/**
- * Marks a field as [Nullable].
- */
+/// Marks a field as [Nullable].
 const Nullable nullable = const Nullable();
