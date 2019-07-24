3cb1459b07e586b0fbdc17edda326e6e1c4a6397
Paul DeMarco <paulmdemarco@gmail.com>
2019-07-24 18:33:49 -0400

    Add some tooling to publishing

---
+++ tools/post-publish.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Remove copied files
+
+rm -rf ./auto_data/example
+rm -rf ./auto_data_generator/example
+
+rm ./auto_data/README.md
+rm ./auto_data_generator/README.md
+
+rm ./auto_data/CHANGELOG.md
+rm ./auto_data_generator/CHANGELOG.md
---
+++ tools/pre-publish.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Copy files into each package to be published
+
+cp -r example auto_data/
+cp -r example auto_data_generator/
+
+cp README.md auto_data/
+cp README.md auto_data_generator/
+
+cp CHANGELOG.md auto_data/
+cp CHANGELOG.md auto_data_generator/
+
